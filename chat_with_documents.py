37de03332ad0fd76902a5955093eaf8071523a10
Dave C. <dave_cropper2003@yahoo.co.uk>
2025-12-18 15:08:47 +1100

    Initial Streamlit RAG app

---
+++ chat_with_documents.py
@@ -0,0 +1,165 @@
+import os
+import streamlit as st
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import Chroma
+
+
+def load_document(file):
+    name, extension = os.path.splitext(file)
+
+    if extension == '.pdf':
+        from langchain_community.document_loaders import PyPDFLoader
+        print(f'Loading {file}')
+        loader = PyPDFLoader(file)
+    elif extension == '.docx':
+        from langchain_community.document_loaders import Docx2txtLoader
+        print(f'Loading {file}')
+        loader = Docx2txtLoader(file)
+    elif extension == '.txt':
+        from langchain_community.document_loaders import TextLoader
+        print(f'Loading {file}')
+        loader = TextLoader(file)
+    else:
+        print('Document format is not supported')
+        return None
+        
+    data = loader.load()
+    return data
+
+
+def chunk_data(data, chunk_size=256, chunk_overlap=20):
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    text_splitter  = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+    chunks = text_splitter.split_documents(data)
+    return chunks
+
+
+def create_embeddings(chunks):
+    embeddings = OpenAIEmbeddings()
+    vector_store = Chroma.from_documents(chunks, embeddings)
+    return vector_store
+
+
+def ask_and_get_answer(vestor_store, q, k=3):
+    from langchain_core.runnables import RunnablePassthrough
+    from langchain_core.output_parsers import StrOutputParser
+    from langchain_openai import ChatOpenAI
+    from langchain_core.prompts import ChatPromptTemplate
+    
+    prompt = ChatPromptTemplate.from_template(
+        """You are a helpful assistant. 
+    Use the context below to answer the question. If the answer is not in the context, say you don't know.
+    
+    Context:
+    {context}
+    
+    Question: {question}"""
+    )
+    
+    # 1. LLM
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
+    
+    # 2. Retriever
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": k}
+    )
+    
+    # 3. LCEL RAG chain
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    
+    # 4. Ask a question
+    result = rag_chain.invoke(q)
+    return result
+
+
+def calculate_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum(
+        [len(enc.encode(page.page_content)) for page in texts]
+    )
+    return total_tokens, total_tokens / 1000 * 0.0004
+
+
+def clear_history():
+    if 'history' in st.session_state:
+        del st.session_state['history']
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv(), override=True)
+
+    st.image('img.png')
+    st.subheader('LLM Questioning-Answering Application')
+
+    with st.sidebar:
+        uploaded_file = st.file_uploader(
+            'Upload a file:',
+            type=['pdf', 'docx', 'txt']
+        )
+        chunk_size = st.number_input(
+            'Chunk size:',
+            min_value=100,
+            max_value=2048,
+            value=512,
+            on_change=clear_history
+        )
+        k = st.number_input(
+            'k',
+            min_value=1,
+            max_value=20,
+            value=3,
+            on_change=clear_history
+        )
+        add_data = st.button('Add Data!', on_click=clear_history)
+
+        if uploaded_file and add_data:
+            with st.spinner('Reading, chunking and embedding file ...'):
+                bytes_data = uploaded_file.read()
+                file_name = os.path.join('./', uploaded_file.name)
+                with open(file_name, 'wb') as f:
+                    f.write(bytes_data)
+
+                data = load_document(file_name)
+                chunks = chunk_data(data, chunk_size=chunk_size)
+                st.write(f'Chunk size: {chunk_size}, Chunks: {len(chunks)}')
+
+                tokens, embedding_cost = calculate_embedding_cost(chunks)
+                st.write(f'Embedding cost: ${embedding_cost:.4f}')
+
+                vector_store = create_embeddings(chunks)
+                st.session_state.vs = vector_store
+
+                st.success('File uploaded, chunked and embedded successfully')
+
+    q = st.text_input('Ask a question about the content of your file:')
+    if q:
+        if 'vs' in st.session_state:
+            vector_store = st.session_state.vs
+            st.write(f'k: {k}')
+            answer = ask_and_get_answer(vector_store, q, k)
+            st.text_area('LLM Answer: ', value=answer)
+
+            st.divider()
+            if 'history' not in st.session_state:
+                st.session_state.history = ''
+            value = f'Q: {q} \nA: {answer}'
+            st.session_state.history = (
+                f'{value} \n {"-" * 100} \n {st.session_state.history}'
+            )
+            st.text_area(
+                label='Chat History',
+                value=st.session_state.history,
+                key='history',
+                height=400
+            )
